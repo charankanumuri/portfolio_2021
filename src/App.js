2b15bd0833a7406033d71c5e8ba309e47f82eb95
charankanumuri <charan.kanumuri@gmail.com>
2021-01-03 00:06:29 +0530

    navbar and header commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Particles from 'react-particles-js'
 function App() {
-	return <h1>PORTFOLIO Mr.VENKATA SAI CHARAN KANUMURI</h1>;
+	return (
+		<>
+      <Particles className='particles-canvas' params={{
+        particles: {
+          number: {
+            value:30,
+            density: {
+              enable: true,
+              value_area:900
+            }
+          },
+          shape:{
+            type: 'circle',
+            stroke:{
+              width:6,
+              color: '#f9ab00'
+            }
+          }
+        }
+      }}/>
+			<Navbar />
+      <Header/>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+const Header = () => {
+	return (
+		<div className="header-wraper">
+			<div className="main-info">
+				<h1>Mern Stack developer and Java Developer </h1>
+				<Typed
+					className="typed-text"
+					strings={[ 'Web Development', 'Spring Boot', 'MERN Stack', 'AWS Certified-SAA-C02' ]}
+					typeSpeed={40}
+					backSpeed={60}
+					loop
+				/>
+				<a href="#" className="btn-main-offer">
+					Contact me
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
